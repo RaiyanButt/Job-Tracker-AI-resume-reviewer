49b2d1754aeb84d70ca711a2e1158845ffd1e2d5
Raiyan Saleem <raiyansaleem@Raiyans-Air.ht.home>
2025-09-17 01:49:04 -0400

    prepared for the deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,19 +5,22 @@ import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
 
-import aiRoutes from "./routes/aiRoutes.js";           
+import aiRoutes from "./routes/aiRoutes.js";  
+import path from "path";         
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
+if(process.env.NODE_ENV !== "production") {
 app.use(
   cors({
     origin: "http://localhost:5173",
   })
 );
-
+}
 app.use(express.json({ limit: "1mb" }));
 
 app.use(rateLimiter);
@@ -27,7 +30,14 @@ app.use("/api/jobs", jobsRoutes);
 
 app.use("/api/ai", aiRoutes);                
 
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  });
+}
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// in production there is no local host so make dynamic
+const BASE_URL= import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
     baseURL: "http://localhost:5001/api",
 });
